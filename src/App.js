523557f193f1e761bfc8ce425ca5176cb55d8edf
Sonu Sharma <sonu.sharma045@gmail.com>
2021-05-09 15:12:01 +0530

    ✨ implemet pricing section design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
+// import { render } from '@testing-library/react';
+
+const plan = {
+  "Basic": {
+    "title":"Basic",
+    "description":"For team and developers",
+    "price":"0",
+    "unit":"/month",
+    "button":"Join for free",
+  },
+  "Team": {
+    "title":"Team",
+    "description":"Advanced collaboration and support for teams.",
+    "price":"2",
+    "unit":"/user/month",
+    "button":"Continue with team",
+  },
+  "Enterprise": {
+    "title":"Enterprise",
+    "description":"Security and flexible deployment",
+    "price":"40",
+    "unit":"/user/month",
+    "button":"Contact Sales",
+  },
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background-stripe">
+
+      </div>
+      <div className="card">
+        
+        <div className="card-header">
+          <h3>
+            Ready to get started ?
+          </h3>
+          <a href="#">View all pricing plans</a>
+        </div>
+
+        <div className="pricing-card-group">
+          <PricingCard plan={plan["Basic"]}/>
+          <PricingCard plan={plan["Team"]}/>
+          <PricingCard plan={plan["Enterprise"]}/>
+        </div>
+
+      </div>
     </div>
   );
+}
+
+function PricingCard(props){
+  return <div className="pricing-card">
+  <h3 className="pricing-card-title"> {props.plan.title}</h3>
+  
+  <p className="body-text">{props.plan.description}</p>
+  
+  <div className="pricing-card-amount">
+    <h1 className="pricing-card-price">${props.plan.price}</h1>
+     <div className="body-text">{props.plan.unit}</div>
+  </div>
+
+  <button className="submit-button">{props.plan.button}</button>
+ 
+</div>
 }
 
 export default App;
